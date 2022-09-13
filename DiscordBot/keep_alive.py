8393871e865c2d8cfef7ba4d00a44a121289ed92
Anthony Brogni <anthonybrog@gmail.com>
2022-09-13 08:41:38 -0500

    Cleanup keep_alive.py

--- DiscordBot/keep_alive.py
+++ DiscordBot/keep_alive.py
@@ -1,17 +1,20 @@
-## Import the necessary modules for this file
+# Import the necessary modules for this file
 from flask import Flask
 from threading import Thread
 
-web = Flask('') ## Create an instance of a 'Flask' object
+web = Flask('')  # Create an instance of a 'Flask' object
+
 
 @web.route('/')
 def home():
-   return "I am alive!"
+    return "I am alive!"
+
 
 def run():
-  web.run(host='0.0.0.0',port=8080)
+    web.run(host='0.0.0.0', port=8080)
 
-## Keep the thread alive
+
+# Keep the thread alive
 def keep_alive():
-   run_thread = Thread(target=run)
-   run_thread.start()
+    run_thread = Thread(target=run)
+    run_thread.start()
